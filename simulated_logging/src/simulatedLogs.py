ab29dd16c7ea26338f3c08bd93b5aa8274d52313
Liz Rice <liz@lizrice.com>
2016-03-26 21:35:00 +0000

    level is a keyword arg for enqueue

--- simulated_logging/src/simulatedLogs.py
+++ simulated_logging/src/simulatedLogs.py
@@ -40,23 +40,23 @@ def simulate():
       change = 1
     elif temp >= _too_hot:
       change = -1
-    msgQueue.enqueue("Change since last reading: " + str(change), "INFO")
+    msgQueue.enqueue("Change since last reading: " + str(change), level="INFO")
 
     temp = temp + change
-    msgQueue.enqueue("Current temperature: " + str(temp), "INFO")
+    msgQueue.enqueue("Current temperature: " + str(temp), level="INFO")
 
     if temp == _just_right:
-      msgQueue.enqueue("That's perfect", "INFO")
+      msgQueue.enqueue("That's perfect", level="INFO")
     elif temp < _just_right and temp > _too_cold:
-      msgQueue.enqueue('Getting a little chilly', "WARNING")
+      msgQueue.enqueue('Getting a little chilly', level="WARNING")
     elif temp > _just_right and temp < _too_hot:
-      msgQueue.enqueue('Getting a touch warm', "WARNING")
+      msgQueue.enqueue('Getting a touch warm', level="WARNING")
     elif temp <= _too_cold:
-      msgQueue.enqueue('Too cold, how did this happen?', "ERROR")
+      msgQueue.enqueue('Too cold, how did this happen?', level="ERROR")
     elif temp >= _too_hot:
-      msgQueue.enqueue('Too hot, how did this happen?', "ERROR")
+      msgQueue.enqueue('Too hot, how did this happen?', level="ERROR")
     else:
-      msgQueue.enqueue('Can''t tell if it''s hot or cold', "ERROR")
+      msgQueue.enqueue('Can''t tell if it''s hot or cold', level="ERROR")
 
     msgQueue.close()
 
